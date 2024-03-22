14590f374d50774e789ae83af65e160ca7bd0cd6
ARJUN-TUDU <64701428+ARJUN-TUDU@users.noreply.github.com>
2024-03-22 22:48:48 +0530

    s

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 app.use(cors({
      
-    origin:["http://localhost:3000"],
+    origin:["https://new-new-1-frontend.vercel.app/"],
     methods:["GET","POST","PUT","DELETE"],
       credentials: true
 
